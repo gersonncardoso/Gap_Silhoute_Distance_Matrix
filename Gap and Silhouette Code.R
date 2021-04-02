5707901dce345e1750f97a0cd42ec1701bc71770
Gerson Nassor Cardoso <36248181+gersonncardoso@users.noreply.github.com>
2021-04-02 18:13:30 -0300

    Update Gap and Silhouette Code.R

--- Gap and Silhouette Code.R
+++ Gap and Silhouette Code.R
@@ -1,80 +1,33 @@
-pkgs <- c("factoextra", "NbClust", "Hmisc, "Igraph","fBasicsp"")
+#Libraries
 library(Hmisc)
 library(factoextra)
 library(NbClust)
-Pre_Crise <- read_excel("D:/Asus Pro/Google Drive/Acad�micos/Dados/Pr� Crise x Pos Crise -2 Anos/Pre Crise/Pre_Crise.xlsx", 
-                        +     col_types = c("blank", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric", "numeric", 
-                                            +         "numeric", "numeric"))
+library(readxl)
+library(cluster)
+
+#Degine currente directory
+setwd("C:/Users/gerso/Google Drive/Acadêmicos/Dados/Pré Crise x Pos Crise -2 Anos/Pre Crise")
+Pre_Crise <- read_excel("C:/Users/gerso/Google Drive/Acadêmicos/Dados/Pré Crise x Pos Crise -2 Anos/Pre Crise/Pre_Crise.xlsx")
+Pre_Crise <- subset(Pre_Crise, select = -c(...1) )
+
+#Distance Correlation - Gower distance
 correlacao=rcorr(as.matrix(Pre_Crise), type="pearson")#Correlation Matrix
 dist_cor=sqrt(2*(1-correlacao$r))#Distance Correlation Matrix
-fviz_nbclust(dist_cor, hcut, method = "silhouette")+# Silhouette method
+
+
+#Clustering Silhoutte - Plot
+fviz_nbclust(dist_cor, kmeans, method = "silhouette")+# Silhouette method
   labs(subtitle = "Silhouette Method")
-#Gap statistic
+
+#Gap statistic - Plot
 set.seed(123)
-fviz_nbclust(dist_cor, hcut, nstart = 25, method = "gap_stat", nboot = 50)+
+fviz_nbclust(dist_cor, kmeans, nstart = 25, method = "gap_stat", nboot = 50)+
   labs(subtitle = "Gap statistic method")
+
+
+# Compute gap statistic for kmeans
+# we used B = 500 interations get the values for max of 20 clusters. Enough to finde the first unitary standard deviation
+gap_stat <- clusGap(dist_cor, FUN = kmeans, nstart = 25,
+                    K.max = 20, B = 500)
+print(gap_stat, method = "firstmax")
+fviz_gap_stat(gap_stat)
